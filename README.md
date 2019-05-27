b84368cb90164715255c009d7c7849873e0c028b
Billcountry <me@billcountry.tech>
2019-05-27 09:21:58 +0300

    Improved installer

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 My implementation of a deezer desktop app on electron. Integrates with the desktop to allow media keys control and background play.
 
 I have no affiliation with (@Deezer)[https://github.com/deezer],just some guy still wondering why there's no official linux app while it takes less than 4 hours to build one
+
+Built using: [Electron Packager](https://www.npmjs.com/package/electron-packager)
--- installer.sh
+++ installer.sh
@@ -1,6 +1,12 @@
 # A simple installer Script that clones the repo and builds your app
-ech "cd /tmp ..."
-cd /tmp
+echo "Starting installation for deezer client"
+cd ~/.local/
+
+echo "Removing older installs if any" 
+[ -e deezer-client ] && rm -r deezer-client
+[ -e share/deezer-client ] && rm -r share/deezer-client
+[ -e share/applications/deezer-client.desktop ] && rm share/applications/deezer-client.desktop
+[ -e bin/deezer-client ] && rm bin/deezer-client
 
 echo "mkdir deezer-client ..."
 mkdir deezer-client
@@ -32,10 +38,10 @@ echo ln -s ~/.local/share/deezer-client/Deezer ~/.local/bin/deezer-client
 echo "cp deezer-client.desktop ~/.local/share/applications ..."
 cp deezer-client.desktop ~/.local/share/applications
 
-echo "cd /tmp ..."
-cd /tmp
+echo "cd ~/.local/ ..."
+cd ~/.local/
 
 echo "rm -r deezer-client ..."
-rm -r deezer-client
+rm -R deezer-client
 
 echo "Yo! good to go ..."
